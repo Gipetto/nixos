eccad08663297088efc3803283509faba00c9783
Shawn Parker <shawn@topfroggraphics.com>
2025-12-26 15:31:21 -0700

    Update

--- common/locale.nix
+++ common/locale.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 {
-  time.timeZone = "America/Los_Angeles";
+  time.timeZone = "America/Denver";
 
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {
