5c00c2d86be272c0837734ef5b85662df0b668a2
Shawn Parker <shawn@topfroggraphics.com>
2026-01-30 09:20:15 -0700

    Fix vscode & add (but dont run) 1Password

---
+++ home-manager/programs/1password.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, ... }:
+{
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [
+      "shawn"
+    ];
+  };
+}
--- home-manager/programs/vscode.nix
+++ home-manager/programs/vscode.nix
@@ -6,41 +6,43 @@ in
   programs.vscode = {
     enable = true;
     package = inputs.nixpkgs-unstable.legacyPackages.${pkgs.system}.vscode; 
-    userSettings = {
-      "accessibility.dimUnfocused.enabled" = true;
-      "accessibility.dimUnfocused.opacity" = 0.8;
-      "debug.javascript.autoAttachFilter" = "disabled";
-      "debug.javascript.automaticallyTunnelRemoteServer" = false;
-      "chat.agent.enabled" = false;
-      "chat.commandCenter.enabled" = false;
-      "editor.fontFamily" = "'Berkeley Mono', 'Monaspace Neon Var', 'Menlo', Monaco, 'Courier New', monospace";
-      "editor.fontLigatures" = "'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'calt', 'dlig'";
-      "editor.fontSize" = 13;
-      "editor.fontVariations" = true;
-      "editor.formatOnSave" = true;
-      "editor.inlayHints.enabled" = "off";
-      "editor.minimap.enabled" = false;
-      "editor.tabSize" = 2;
-      "files.insertFinalNewline" = true;
-      "svelte.plugin.svelte.defaultScriptLanguage" = "ts";
-      "svg.preview.mode" = "svg";
-      "terminal.integrated.fontSize" = "13";
-      "workbench.colorTheme" = "Default Light+";
+    profiles.default = {
+      userSettings = {
+        "accessibility.dimUnfocused.enabled" = true;
+        "accessibility.dimUnfocused.opacity" = 0.8;
+        "debug.javascript.autoAttachFilter" = "disabled";
+        "debug.javascript.automaticallyTunnelRemoteServer" = false;
+        "chat.agent.enabled" = false;
+        "chat.commandCenter.enabled" = false;
+        "editor.fontFamily" = "'Berkeley Mono', 'Monaspace Neon Var', 'Menlo', Monaco, 'Courier New', monospace";
+        "editor.fontLigatures" = "'ss01', 'ss02', 'ss03', 'ss04', 'ss05', 'ss06', 'ss07', 'ss08', 'calt', 'dlig'";
+        "editor.fontSize" = 13;
+        "editor.fontVariations" = true;
+        "editor.formatOnSave" = true;
+        "editor.inlayHints.enabled" = "off";
+        "editor.minimap.enabled" = false;
+        "editor.tabSize" = 2;
+        "files.insertFinalNewline" = true;
+        "svelte.plugin.svelte.defaultScriptLanguage" = "ts";
+        "svg.preview.mode" = "svg";
+        "terminal.integrated.fontSize" = "13";
+        "workbench.colorTheme" = "Default Light+";
+      };
+      extensions = [
+        vscode-extensions.denoland.vscode-deno
+        vscode-extensions.docker.docker
+        vscode-extensions.eamodio.gitlens
+        vscode-extensions.ms-python.autopep8
+        vscode-extensions.ms-python.black-formatter
+        vscode-extensions.ms-python.debugpy
+        vscode-extensions.ms-python.python
+        vscode-extensions.ms-vscode-remote.remote-containers
+        vscode-extensions.ms-vscode-remote.remote-ssh
+        vscode-extensions.ms-vscode-remote.remote-ssh-edit
+        vscode-extensions.ms-vscode.makefile-tools
+        vscode-extensions.ms-vscode.remote-explorer
+        vscode-extensions.svelte.svelte-vscode
+      ];
     };
-    extensions = [
-      vscode-extensions.denoland.vscode-deno
-      vscode-extensions.docker.docker
-      vscode-extensions.eamodio.gitlens
-      vscode-extensions.ms-python.autopep8
-      vscode-extensions.ms-python.black-formatter
-      vscode-extensions.ms-python.debugpy
-      vscode-extensions.ms-python.python
-      vscode-extensions.ms-vscode-remote.remote-containers
-      vscode-extensions.ms-vscode-remote.remote-ssh
-      vscode-extensions.ms-vscode-remote.remote-ssh-edit
-      vscode-extensions.ms-vscode.makefile-tools
-      vscode-extensions.ms-vscode.remote-explorer
-      vscode-extensions.svelte.svelte-vscode
-    ];
   };
 }
