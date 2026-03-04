77de5adb738bceadae343aea9c40586e563b162a
Shawn Parker <shawn@topfroggraphics.com>
2026-03-04 13:52:48 -0700

    Set path in .zprofile in darwin to appease vscode

--- home-manager/darwin.nix
+++ home-manager/darwin.nix
@@ -1,12 +1,19 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
-  imports = [ 
-    ./common.nix 
+  imports = [
+    ./common.nix
   ];
 
   home.packages = with pkgs; [
     _1password-cli
     docker-client
   ];
+
+  # ensure that pathing is correct when loading login shells in vscode
+  home.activation.zprofileNixPath = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    if ! grep -q 'nix-profile/bin' "$HOME/.zprofile" 2>/dev/null; then
+      echo 'path=("$HOME/.nix-profile/bin" $path)' >> "$HOME/.zprofile"
+    fi
+  '';
 }
